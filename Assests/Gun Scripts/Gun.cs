2ce16cc579d9881e6d8e6b13c9f712614621a7e0
kavi0000 <76543632+kavi0000@users.noreply.github.com>
2021-09-01 10:46:54 +0530

    Add files via upload

---
+++ Assests/Gun Scripts/Gun.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 25f;
+
+    public float range = 10f;
+
+    public Camera fpsCam;
+
+    public Animator animator;
+
+    private bool isAttack = false;
+
+    public AudioSource attackSound;
+
+    void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+            isAttack = !isAttack;
+            animator.SetBool("Attack", isAttack);
+            attackSound.Play();
+            
+        }  else if (Input.GetButtonUp("Fire1"))
+        {
+            isAttack = !isAttack;
+            animator.SetBool("Attack", isAttack);
+        }
+        
+    }
+
+    void Shoot ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+
+    
+
+
+}
