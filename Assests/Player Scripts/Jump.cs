30f4c078ec8d02386768ca6840f078795763ca96
kavi0000 <76543632+kavi0000@users.noreply.github.com>
2021-09-01 10:55:20 +0530

    Add files via upload

---
+++ Assests/Player Scripts/Jump.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public bool isGrounded = true;
+
+    public AudioSource jumpSound;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);           
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter (Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            jumpSound.Play();
+        }
+    }
+
+    
+}
---
+++ Assests/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float speed = 12f;
+
+    void Update ()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+        float z = Input.GetAxis("Vertical") * speed;
+
+        rb.velocity = (transform.right * x) + (transform.forward * z) + (transform.up * rb.velocity.y);
+    }
+}
