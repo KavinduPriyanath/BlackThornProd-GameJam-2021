cd86a1f5c265a8508cd4e67106a2211f825e55b0
kavi0000 <76543632+kavi0000@users.noreply.github.com>
2021-09-01 10:44:09 +0530

    Add files via upload

---
+++ Assests/Death Menus/DeathMenu01more.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu01more : MonoBehaviour
+{
+
+    public void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void SceneChange ()
+    {
+        SceneManager.LoadScene("Level 01 more");
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assests/Death Menus/DeathMenu02more.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathMenu02more : MonoBehaviour
+{
+
+    public void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void SceneChange ()
+    {
+        SceneManager.LoadScene("Level 02 more");
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
